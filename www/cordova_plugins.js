f757d132ee0104ff55f9784c8617d01696ad8031
gndvr2010 <gndvr2010@gmail.com>
2015-10-24 17:06:21 -0300

    pruebas

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -1,11 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
-    },
-    {
         "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
         "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
         "clobbers": [
@@ -13,19 +8,16 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/plugin.socket.io/www/socketio.js",
-        "id": "plugin.socket.io.SocketIO",
-        "clobbers": [
-            "plugin.socket.io"
-        ]
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0",
     "phonegap-plugin-barcodescanner": "4.1.0",
-    "plugin.socket.io": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
