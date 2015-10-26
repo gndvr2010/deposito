a2ee6880211b956c72e0a97b14f72e0877027fb8
gndvr2010 <gndvr2010@gmail.com>
2015-10-26 09:38:15 -0300

    whitelist

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,13 +6,19 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.barcodeScanner"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "phonegap-plugin-barcodescanner": "4.1.0",
-    "cordova-plugin-crosswalk-webview": "1.4.0-dev"
+    "cordova-plugin-crosswalk-webview": "1.4.0-dev",
+    "cordova-plugin-whitelist": "1.1.1-dev"
 }
 // BOTTOM OF METADATA
 });
